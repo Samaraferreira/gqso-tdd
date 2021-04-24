5d20996e19fde2477cd98226f531042fd52a8d3d
Samara Ferreira <ferreirasamara.sf@gmail.com>
2021-04-23 21:57:33 -0300

    test: (fail) deve retornar saldo incrementado ao fazer deposito

--- src/main/java/br/edu/ifal/contabancaria/Conta.java
+++ src/main/java/br/edu/ifal/contabancaria/Conta.java
@@ -1,2 +1,10 @@
-package br.edu.ifal.contabancaria;public class ContaBancaria {
+package br.edu.ifal.contabancaria;
+
+public class Conta {
+
+    private double saldo = 0.0;
+
+    public double deposito(double valor) {
+        return 0;
+    }
 }
--- src/test/java/br/edu/ifal/contabancaria/ContaTest.java
+++ src/test/java/br/edu/ifal/contabancaria/ContaTest.java
@@ -1,2 +1,22 @@
-package br.edu.ifal.contabancaria;public class ContaBancaria {
+package br.edu.ifal.contabancaria;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContaTest {
+
+    private Conta conta;
+
+    @BeforeEach
+    public void setUp() {
+        this.conta = new Conta();
+    }
+
+    @Test
+    public void deveRetornarSaldoIncrementadoAoFazerDeposito() {
+        assertEquals(10, conta.deposito(10));
+    }
+
 }
